ca6171c9fde2644e9df94622eb2b7902c995279e
ganzan <lorbelleganzan@gmail.com>
2024-04-29 19:20:32 +0800

    added initials

--- db/ganzan.sql
+++ db/ganzan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 22, 2024 at 11:35 AM
+-- Generation Time: Apr 29, 2024 at 01:10 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,35 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `employees`
+-- Table structure for table `products`
 --
 
-CREATE TABLE `employees` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `salary` int(10) NOT NULL
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_thumbnail_link` varchar(255) DEFAULT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_description` varchar(100) NOT NULL,
+  `product_retail_price` varchar(255) NOT NULL,
+  `product_date_added` date NOT NULL,
+  `product_updated_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'raito', '$2y$10$0W9xK0LRnCcIoI/zsHTaVe5qTvjkzePpkwzv048Li0numNM2vxNUS', '2024-04-29 18:37:22'),
+(2, 'test123', '$2y$10$BQuxUMsbbuc.ii4UUY3xwub3Xnc19KtUjhu5NolLmgj1ZjrXD2nMC', '2024-04-29 18:37:36'),
+(3, 'test123456', '$2y$10$zcjcQXD7zMiqhBO3jjIr.uktMiAilFDcrEWLRWc6s/BdMSg3kux/G', '2024-04-29 18:38:59'),
+(4, 'raito12344', '$2y$10$gwTeE8l08WfU.7Cs9VS/zeMo8f/okVEz3k1NZvYUF/x9mu28dLU.G', '2024-04-29 18:51:28'),
+(5, 'iii', '$2y$10$ea6IoA/MsuiPbPeMA95RFOMZAu4HoYAUACDEnhIRgqDFMVlPjk/Bu', '2024-04-29 18:53:57'),
+(6, 'ca', '$2y$10$U/Fib7yU7VxWY52Lux.llOOwHP8PXFCuKaOnPVURaTHilwbpXK/wG', '2024-04-29 18:58:04');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `employees`
+-- Indexes for table `products`
 --
-ALTER TABLE `employees`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `employees`
+-- AUTO_INCREMENT for table `products`
 --
-ALTER TABLE `employees`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 }
  
 // Include config file
-require_once "index.php";
+require_once "./db/config.php";
  
 // Define variables and initialize with empty values
 $username = $password = "";
@@ -62,7 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                             // Redirect user to welcome page
-                            header("location: ../public/welcome.php");
+                            header("location: ./public/welcome.php");
                         } else{
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
@@ -122,7 +122,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
-            <p>Don't have an account? <a href="../public/register.php">Sign up now</a>.</p>
+            <p>Don't have an account? <a href="./public/register.php">Sign up now</a>.</p>
         </form>
     </div>
 </body>
