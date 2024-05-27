c50b7cf81ca25bee3774629449b5f3a4dd1af4a9
ganzan <ganzan@gmail.com>
2024-05-27 09:35:23 +0800

    start fixing

--- products/config.php
+++ products/config.php
@@ -1,18 +1,19 @@
 <?php
 
 $host = 'localhost';
-$dbname = 'ganzan';
+$dbname = 's';
 $username = 'root';
 $password = '';
 /*
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'u593341949_dev_demo');
-define('DB_PASSWORD', 'Admin_demo123');
-define('DB_NAME', 'u593341949_db_demo');
+$host = 'localhost';
+$dbname = 'u593341949_db_quidit';
+$username = 'u593341949_dev_quidit';
+$password = '20212014Quidit';
 */
-try {
+try {   
  $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
  die("Database connection failed: " . $e->getMessage());
 }
+
